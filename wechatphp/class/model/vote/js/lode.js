dd4eedddd64b446c50f49304a4cd8dbb613ea1e5
wfcsnzdm <530254427@qq.com>
2014-12-07 09:08:11 +0800

    vote
    vote~

---
+++ wechatphp/class/model/vote/js/lode.js
@@ -0,0 +1,61 @@
+function lode() {
+	//var urlpc = "http://wx.ecjtu.net/wmkj/css/pc.css";
+	var dynamicLoading = {
+    	css: function(path){
+			if(!path || path.length === 0){
+				throw new Error('argument "path" is required !');
+			}
+			var head = document.getElementsByTagName('head')[0];
+        	var link = document.createElement('link');
+        	link.href = path;
+        	link.rel = 'stylesheet';
+        	link.type = 'text/css';
+        	//console.log(link);
+        	head.appendChild(link);
+    	},
+    	js: function(path){
+			if(!path || path.length === 0){
+				throw new Error('argument "path" is required !');
+			}
+			var head = document.getElementsByTagName('head')[0];
+    	    var script = document.createElement('script');
+    	    script.src = path;
+   	    	script.type = 'text/javascript';
+   	    	head.appendChild(script);
+   	 	}
+	}
+	var browser = function () {
+        var w = window,ver = w.opera ? (opera.version().replace(/\d$/, "") - 0)
+            : parseFloat((/(?:IE |fox\/|ome\/|ion\/)(\d+\.\d)/.
+                exec(navigator.userAgent) || [,0])[1]);
+            return {
+                //测试是否为ie或内核为trident，是则取得其版本号
+                ie: !!w.VBArray && Math.max(document.documentMode||0, ver),//内核trident
+                //测试是否为firefox，是则取得其版本号
+                firefox: !!w.netscape && ver,//内核Gecko
+                //测试是否为opera，是则取得其版本号
+                opera:  !!w.opera && ver,//内核 Presto 9.5为Kestrel 10为Carakan
+                //测试是否为chrome，是则取得其版本号
+                chrome: !! w.chrome &&  ver ,//内核V8
+                //测试是否为safari，是则取得其版本号
+                safari: /apple/i.test(navigator.vendor) && ver,// 内核 WebCore
+                
+                isIphone: /iPhone/i.test( navigator.userAgent ),
+                isIpad: /iPad/i.test( navigator.userAgent ),
+                isAndroid: /android/i.test( navigator.userAgent ),
+                //isPhone: isIphone || isAndroid
+            }
+    }
+    var is = browser();
+    //console.log(is);
+    if(!is['isIpone']&&!is['isAndroid']){
+    	dynamicLoading.css("./css/pc.css");
+    }
+    
+}
+
+HTMLElement.prototype.addEventListener.apply(window.document, ['DOMContentLoaded', lode, false]);
+
+
+
+  
---
+++ wechatphp/class/model/vote/submit.php
@@ -0,0 +1,209 @@
+<?php
+	session_start();
+	if($_SESSION['username'] == NULL){
+		 header("Location: sign_new.htm"); 
+		 exit(0);
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ForPP</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="http://libs.baidu.com/bootstrap/2.3.1/css/bootstrap.min.css" rel="stylesheet">
+<link href="http://cdn.bootcss.com/bootstrap/2.3.1/css/bootstrap-responsive.min.css" rel="stylesheet">
+<link href="http://cdn.bootcss.com/bootstrap/3.1.1/css/bootstrap-theme.css" rel="stylesheet">
+<link rel="stylesheet" href="http://blueimp.github.io/Gallery/css/blueimp-gallery.min.css">
+<link rel="stylesheet" href="css/jquery.fileupload.css">
+<link rel="stylesheet" href="css/jquery.fileupload-ui.css">
+<style type="text/css" rel="stylesheet">
+	body {
+	padding-top: 40px;
+	padding-bottom: 40px;
+	background-color: #f5f5f5;
+}
+.mainbox{
+	padding: 19px 29px 29px;
+	margin: 0 auto 20px;
+	background-color: #fff;
+	border: 1px solid #e5e5e5;
+	-webkit-border-radius: 5px;
+	-moz-border-radius: 5px;
+	border-radius: 5px;
+	-webkit-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+	-moz-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+	box-shadow: 0 1px 2px rgba(0,0,0,.05);
+}
+.support {
+	width: 100%;
+	max-width: 300px;
+	margin: 0 auto;
+	color: #222;
+	text-align: center;
+}
+.fileinput-button {
+  position: relative;
+  overflow: hidden;
+}
+.fileinput-button input {
+  position: absolute;
+  top: 0;
+  right: 0;
+  margin: 0;
+  opacity: 0;
+  -ms-filter: 'alpha(opacity=0)';
+  font-size: 200px;
+  direction: ltr;
+  cursor: pointer;
+}
+</style>
+<link rel="shortcut icon" href="../image/logo_icon.png" type="image/png"> 
+</head>
+<body>
+<div class="container">
+	<div class="row-fluid">
+		<div class="span10 offset1 ">
+				<div class="mainbox">
+					<legend><strong>Content Publishing</strong> <small>内容发布</small></legend>
+					   <div class="control-group title">
+						<lable for="title">标题：</lable>
+						<input type="text"  placeholder="标题(50个字符内)" name="title"/>
+						<span class="help-block"></span>
+					   </div>
+					   <div class="control-group desc">
+					    <lable for="desc">描述：</lable>
+						<input type="text"  placeholder="描述(255个字符内)" name="desc"/>
+					   	<span class="help-block"></span>
+					   </div>
+    					<form id="fileupload" action="//jquery-file-upload.appspot.com/" method="POST" enctype="multipart/form-data">
+        <noscript><input type="hidden" name="redirect" value="http://blueimp.github.io/jQuery-File-Upload/"></noscript>
+        <div class="fileupload-buttonbar">
+            <div class="col-lg-7">
+                <span class="btn btn-success fileinput-button">
+                    <i class="glyphicon glyphicon-plus"></i>
+                    <span>添加文件...</span>
+                    <input type="file" name="files[]" multiple>
+                </span>
+                <span class="danger">(注意：为方便管理,文件上传后将会更名。)</span>
+                <span class="fileupload-process"></span>
+            </div>
+           
+            <div class="col-lg-5 fileupload-progress fade">
+                <div class="progress-extended">&nbsp;</div>
+            </div>
+        </div>
+        <table role="presentation" class="table table-striped"><tbody class="files"></tbody></table>
+    </form>
+    <br>
+    
+<div id="blueimp-gallery" class="blueimp-gallery blueimp-gallery-controls" data-filter=":even">
+    <div class="slides"></div>
+    <h3 class="title"></h3>
+    <a class="prev">‹</a>
+    <a class="next">›</a>
+    <a class="close">×</a>
+    <a class="play-pause"></a>
+    <ol class="indicator"></ol>
+</div>
+<script id="template-upload" type="text/x-tmpl">
+{% for (var i=0, file; file=o.files[i]; i++) { %}
+    <tr class="template-upload fade">
+        <td>
+            <span class="preview"></span>
+        </td>
+        <td>
+            <p class="name">{%=file.name%}</p>
+            <strong class="error text-danger"></strong>
+        </td>
+        <td>
+            <p class="size"> 处理中...</p>
+            <div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0"><div class="bar" style="width:0%;"></div></div>
+        </td>
+        <td>
+            {% if (!i && !o.options.autoUpload) { %}
+                <button class="btn btn-primary start" disabled>
+                    <i class="glyphicon glyphicon-upload"></i>
+                    <span>上传</span>
+                </button>
+            {% } %}
+            {% if (!i) { %}
+                <button class="btn btn-warning cancel">
+                    <i class="glyphicon glyphicon-ban-circle"></i>
+                    <span>取消</span>
+                </button>
+            {% } %}
+        </td>
+    </tr>
+{% } %}
+</script>
+<div class="control-group file">
+<script id="template-download" type="text/x-tmpl">
+{% for (var i=0, file; file=o.files[i]; i++) { %}
+    <tr class="template-download fade">
+        <td>
+            <span class="preview">
+                {% if (file.thumbnailUrl) { %}
+                    <a href="{%=file.url%}" title="{%=file.name%}" download="{%=file.name%}" data-gallery><img src="{%=file.thumbnailUrl%}"></a>
+                {% } %}
+            </span>
+        </td>
+        <td>
+            <p class="name">
+                {% if (file.url) { %}
+                	<input name="audio" class="audio" type="checkbox" value="{%=file.url%}"/>
+                    <a href="{%=file.url%}" title="{%=file.name%}" download="{%=file.name%}" {%=file.thumbnailUrl?'data-gallery':''%}>{%=file.name%}{% if(file.pre_name){ %}({%=file.pre_name%}){% } %}</a>
+                {% } else { %}
+                    <span>{%=file.name%}</span>
+                {% } %}
+            </p>
+            {% if (file.error) { %}
+                <div><span class="label label-danger">Error</span> {%=file.error%}</div>
+            {% } %}
+        </td>
+        <td>
+            <span class="size">{%=o.formatFileSize(file.size)%}</span>
+        </td>
+        <td>
+            {% if (file.deleteUrl) { %}
+                <button class="btn btn-danger delete" data-type="{%=file.deleteType%}" data-url="{%=file.deleteUrl%}"{% if (file.deleteWithCredentials) { %} data-xhr-fields='{"withCredentials":true}'{% } %}>
+                    <i class="glyphicon glyphicon-trash"></i>
+                    <span>删除</span>
+                </button>
+            {% } else { %}
+                <button class="btn btn-warning cancel">
+                    <i class="glyphicon glyphicon-ban-circle"></i>
+                    <span>取消</span>
+                </button>
+            {% } %}
+        </td>
+    </tr>
+{% } %}
+</script>
+<span class="help-block"></span>
+</div>
+						<button class="btn btn-large btn-primary submit" type="submit" name="apply" value="apply">提交</button><span class="submit-help"></span><a class="submit-help-a" href='http://wx.ecjtu.net/wmkj/mp3/'></a>						
+				</div>
+			<p class="support">技术支持 <a href="http://www.ecjtu.net"><img src="../image/logo_icon.png" alt="^_^">日新网技术研发中心</a></p>
+		</div>
+	</div>
+</div>
+<script src="http://libs.baidu.com/jquery/2.0.0/jquery.min.js"></script>
+<script src="js/vendor/jquery.ui.widget.js"></script>
+<script src="http://blueimp.github.io/JavaScript-Templates/js/tmpl.min.js"></script>
+<script src="js/cors/load-image.js"></script>
+<script src="http://blueimp.github.io/JavaScript-Canvas-to-Blob/js/canvas-to-blob.min.js"></script>
+<script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+<script src="http://blueimp.github.io/Gallery/js/jquery.blueimp-gallery.min.js"></script>
+<script src="js/jquery.iframe-transport.js"></script>
+<script src="js/jquery.fileupload.js"></script>
+<script src="js/jquery.fileupload-process.js"></script>
+<script src="js/jquery.fileupload-image.js"></script>
+<script src="js/jquery.fileupload-audio.js"></script>
+<script src="js/jquery.fileupload-validate.js"></script>
+<script src="js/jquery.fileupload-ui.js"></script>
+<script src="js/main.js"></script>
+<!--[if (gte IE 8)&(lt IE 10)]>
+<script src="js/cors/jquery.xdr-transport.js"></script>
+<![endif]-->
+</body> 
+</html>
