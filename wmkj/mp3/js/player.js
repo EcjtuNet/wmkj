c74f1c42076495af6f353aa93fb440d17771c91e
homker <liu15jia@163.com>
2015-03-11 20:22:57 +0800

    new mp3 player

---
+++ wmkj/mp3/js/player.js
@@ -0,0 +1,29 @@
+(function(){
+	"use strict"
+	var musicGround = $("#music-ground"),
+		player = $("#player")[0],
+		playerControl = $("#player-controls-play"),
+		mask = true,
+		isClose = true;
+	playerControl.click(function(){
+		console.log(this);
+		if(mask){
+			musicGround.addClass("spinner");
+			playerControl.removeClass("icon-play3").addClass("icon-pause2");
+			mask = false;
+			console.log(player.play());
+		}else{
+			musicGround.removeClass("spinner");
+			playerControl.removeClass("icon-pause2").addClass("icon-play3");
+			mask = true;
+			player.pause();
+		}
+	});
+	$(".list").click(function(){
+		console.log("hehe");
+		$('.showlist').show().animate({width:"150px",height:"100px"},"slow");
+		isClose = false;
+	});
+	$(document).click(function (event) { isClose&&$('.showlist').slideUp('slow');isClose = true; });  
+	$('.showlist').click(function (event) { isClose&&$(this).fadeOut(1000);isClose=true; });  
+})();
