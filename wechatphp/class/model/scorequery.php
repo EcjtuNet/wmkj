fbae622a36295716ceffd16c93f6cf108f770099
wfcsnzdm <530254427@qq.com>
2015-07-12 23:35:31 +0800

    change

--- wechatphp/class/model/scorequery.php
+++ wechatphp/class/model/scorequery.php
@@ -16,7 +16,7 @@ class scorequery extends AbstractQuery
 		$this->_StudentID = $studentID;
 	}
 	public function progress(){
-		$curl_errno = $this->getScore();
+		$curl_errno = $this->getScoreFromPython();
 		//$output = $this->_output;
 		$output = $this->_output;//$this->getScoreFromPython();
 		//return $out = $output;
@@ -94,7 +94,7 @@ class scorequery extends AbstractQuery
 	{
 		//$a = "python ./scoreforwx.py".$this->_StudentID."";
 		$output = exec("python /home/data/www/wx_ecjtu_net/wechatphp/class/model/scoreforwx.py $this->_StudentID");
-		interface_log(DEBUG, 110, var_export("python ./scoreforwx.py $this->_StudentID",true));
+		interface_log(DEBUG, 0, var_export("python ./scoreforwx.py $this->_StudentID",true));
 		return json_decode($output);
 	}
 	
